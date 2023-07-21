949fb1b4d750fa5bbbf76304337d56ec64ab01aa
miguel portatil <miguel.bol3456@gmail.com>
2023-07-21 15:53:59 -0500

    actualizacion del json

--- servicios/skills-servicios.js
+++ servicios/skills-servicios.js
@@ -4,16 +4,12 @@ fetch("https://miguel-bolanos.github.io/Challenge-Front-End-Portafolio/Skills/Sk
 .then(respuesta => respuesta.json())
 .catch(error => console.log("no se conecto" + error))
     
-const listaHobbies = () => 
-fetch("https://miguel-bolanos.github.io/Challenge-Front-End-Portafolio/Skills/Skills.json/Hobbies")
-.then(respuesta => console.log("conectado"))
-.catch(error => console.log("no se conecto" + error))
+
 
 
 export const Servicios  = {
 
     listaSkisll,
-    listaHobbies,
+
 }
 
-listaHobbies()
